ecea6b11e811564cbe83022f670e6c606ad62718
Byron Gonzalez <byrondein@gmail.com>
2024-10-26 23:22:05 -0300

    feat-add-technologies-section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,12 +12,15 @@ const About = () => {
                     <div className="flex items-center justify-center">
                         <img className='rounded-2xl' src={aboutImg} alt="" />
                     </div>
-                    <div className="w-full lg:w-1/2 ">
-                    <div className="flex justify-center lg:justify-center">
-                        {ABOUT_TEXT}
-                    </div>
-                    </div>
+                   
                 </div>
+                <div className="w-full lg:w-1/2 ">
+                        <div className="flex justify-center lg:justify-center">
+                            <p className='my-2 max-w-xl py-6'>
+                                {ABOUT_TEXT}
+                            </p>
+                        </div>
+                    </div>
             </div>
         </div>
     )
