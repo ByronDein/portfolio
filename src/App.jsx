18e92ff6b29794cca7bb9c070ad0bb6f4eafa8cf
Byron Gonzalez <byrondein@gmail.com>
2024-10-29 23:08:36 -0300

    feat-add-contact-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
@@ -20,6 +21,7 @@ const App = () => {
                 <Technologies />
                 <Experience />
                 <Projects />
+                <Contact />
             </div>
 
         </div>
